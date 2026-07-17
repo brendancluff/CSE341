81c322c791dba394f757bdce4b75af2a737d5917
Brendan Cluff <cluffbrendan04@gmail.com>
2026-07-17 14:20:11 -0700

    Complete Contacts Part 2 API and Swagger documentation

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,23 @@
 const mongodb = require('../data/database');
 const { ObjectId } = require('mongodb');
 
+const requiredFields = [
+  'firstName',
+  'lastName',
+  'email',
+  'favoriteColor',
+  'birthday'
+];
+
+const validateContact = (body) => {
+  return requiredFields.every(
+    (field) =>
+      body[field] !== undefined &&
+      body[field] !== null &&
+      String(body[field]).trim() !== ''
+  );
+};
+
 const getAll = async (req, res) => {
   try {
     const contacts = await mongodb
@@ -48,7 +65,127 @@ const getSingle = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  try {
+    if (!validateContact(req.body)) {
+      return res.status(400).json({
+        message:
+          'firstName, lastName, email, favoriteColor, and birthday are required.'
+      });
+    }
+
+    const contact = {
+      firstName: req.body.firstName.trim(),
+      lastName: req.body.lastName.trim(),
+      email: req.body.email.trim(),
+      favoriteColor: req.body.favoriteColor.trim(),
+      birthday: req.body.birthday.trim()
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .collection('contacts')
+      .insertOne(contact);
+
+    if (!response.acknowledged) {
+      return res.status(500).json({
+        message: 'Contact could not be created.'
+      });
+    }
+
+    res.status(201).json({
+      id: response.insertedId
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Unable to create contact.'
+    });
+  }
+};
+
+const updateContact = async (req, res) => {
+  try {
+    const contactId = req.params.id;
+
+    if (!ObjectId.isValid(contactId)) {
+      return res.status(400).json({
+        message: 'Invalid contact ID.'
+      });
+    }
+
+    if (!validateContact(req.body)) {
+      return res.status(400).json({
+        message:
+          'firstName, lastName, email, favoriteColor, and birthday are required.'
+      });
+    }
+
+    const contact = {
+      firstName: req.body.firstName.trim(),
+      lastName: req.body.lastName.trim(),
+      email: req.body.email.trim(),
+      favoriteColor: req.body.favoriteColor.trim(),
+      birthday: req.body.birthday.trim()
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .collection('contacts')
+      .updateOne(
+        { _id: new ObjectId(contactId) },
+        { $set: contact }
+      );
+
+    if (response.matchedCount === 0) {
+      return res.status(404).json({
+        message: 'Contact not found.'
+      });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Unable to update contact.'
+    });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const contactId = req.params.id;
+
+    if (!ObjectId.isValid(contactId)) {
+      return res.status(400).json({
+        message: 'Invalid contact ID.'
+      });
+    }
+
+    const response = await mongodb
+      .getDatabase()
+      .collection('contacts')
+      .deleteOne({ _id: new ObjectId(contactId) });
+
+    if (response.deletedCount === 0) {
+      return res.status(404).json({
+        message: 'Contact not found.'
+      });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Unable to delete contact.'
+    });
+  }
+};
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 require('dotenv').config();
 
 const database = require('./data/database');
@@ -11,16 +14,19 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Contacts API is running.');
 });
 
 app.use('/contacts', require('./routes/contacts'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 database
   .initDb()
   .then(() => {
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
+      console.log(`Swagger documentation: http://localhost:${port}/api-docs`);
     });
   })
   .catch((error) => {
